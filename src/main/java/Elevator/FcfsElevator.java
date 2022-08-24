d0f6eb901bf89692d907dd1dd0efd2876014b4fe
jchyczynski <jasiek.ch16@gmail.com>
2022-08-24 20:53:30 +0200

    First come first serve elevator class and unit tests

---
+++ src/main/java/Elevator/FcfsElevator.java
@@ -0,0 +1,37 @@
+package Elevator;
+
+import static java.lang.Math.abs;
+
+public class FcfsElevator extends Elevator{
+    protected FcfsElevator(int startFloor) {
+        super(startFloor);
+    }
+
+    @Override
+    public int callCost(int floor, Direction direction) {
+        int currentFloor = this.floor;
+        if(destinations.isEmpty()){
+            return abs(floor - currentFloor);
+        }
+        else {
+            int cost = 0;
+            int prevFloor = currentFloor;
+            for(int nextFloor : destinations){
+                cost += abs(prevFloor - nextFloor) + PICKUP_COST;
+                prevFloor = nextFloor;
+            }
+            cost += abs(destinations.get(destinations.size()-1) - floor);
+            return cost;
+        }
+    }
+
+    @Override
+    public void call(int floor, Direction direction) {
+        destinations.add(floor);
+    }
+
+    @Override
+    public void chooseDestination(int floor) {
+        destinations.add(floor);
+    }
+}
---
+++ src/test/java/Elevator/FcfsElevatorTest.java
@@ -0,0 +1,46 @@
+package Elevator;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FcfsElevatorTest {
+    @Test
+    public void shouldQueueDestinations(){
+        FcfsElevator elevator = new FcfsElevator(0);
+        assert (elevator.getDestinations().isEmpty());
+        elevator.call(3, Direction.UP);
+        assertEquals(elevator.getDestinations(), new LinkedList<>(Arrays.asList(3)));
+        elevator.call(2, Direction.UP);
+        assertEquals(elevator.getDestinations(), new LinkedList<>(Arrays.asList(3, 2)));
+        elevator.chooseDestination(4);
+        assertEquals(elevator.getDestinations(), new LinkedList<>(Arrays.asList(3, 2, 4)));
+        elevator.call(1, Direction.UP);
+        assertEquals(elevator.getDestinations(), new LinkedList<>(Arrays.asList(3, 2, 4, 1)));
+        elevator.chooseDestination(6);
+        assertEquals(elevator.getDestinations(), new LinkedList<>(Arrays.asList(3, 2, 4, 1, 6)));
+
+    }
+
+    @Test
+    public void shouldCalculateCost(){
+        FcfsElevator elevator = new FcfsElevator(0);
+        assert (elevator.getDestinations().isEmpty());
+        elevator.call(3, Direction.UP);
+        assertEquals(12, elevator.callCost(10, Direction.UP), 12);
+        elevator.call(2, Direction.UP);
+        assertEquals(8, elevator.callCost(2, Direction.UP));
+        assertEquals(11, elevator.callCost(5, Direction.UP));
+        elevator.chooseDestination(4);
+        assertEquals(13, elevator.callCost(5, Direction.UP));
+        elevator.call(1, Direction.UP);
+        assertEquals(18, elevator.callCost(0, Direction.UP));
+        elevator.chooseDestination(6);
+        assertEquals(30, elevator.callCost(0, Direction.UP));
+    }
+
+}
